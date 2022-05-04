f06859f966424522acd996f1a0b7c13555e0bca1
[YOUNES ADI] <younesadi18@gmail.com>
2022-05-04 17:41:07 +0100

    DAY-7-2

--- script.js
+++ script.js
@@ -95,40 +95,56 @@
 //     w[1].style.color='red';
 // })
 
-// var h=`<div class="main-nav">
-// <a href="#acceuil" class="main-nav-logo active">
-//     <img src="../Logos/MC-logo-light.svg" alt="" width="0" height="0">
-// </a>
-// <ul class="main-nav-elements" id="main-nav-elements">
-//     <li class="main-nav-element">
-//         <a class="nav-page" href="index.html#Welcome">Accueil</a>
-//     </li>
-//     <li class="main-nav-element main-nav-element-hover">
-//         <a href="Presentation.html" class="nav-page">Présentation</a>
-//         <ul class="sub-nav-elements" id="sub-nav-elements">
-//             <li class="sub-nav-element">
-//                 <a href="Presentation.html#Who">qui sommes nous</a>
-//             </li>
-//             <li class="sub-nav-element">
-//                 <a href="Presentation.html#History">historique du club</a>
-//             </li>
-//             <li class="sub-nav-element">
-//                 <a href="Presentation.html#Activities">activités du club</a>
-//             </li>
-//         </ul>
-//     </li>
-//     <li class="main-nav-element">
-//         <a href="Events.html" class="nav-page">Evènements</a>
-//     </li>
-//     <li class="main-nav-element">
-//         <a href="Contact.css" class="nav-page">Contact</a>
-//     </li>
-//     <button class="reg-button">S'inscrire<i class="fas fa-arrow-right"></i></button>
-// </ul>
-// </div>
-// `
+var h=`<div class="main-nav">
+<a href="#acceuil" class="main-nav-logo active">
+    <img src="Logos/MC-logo-light.svg" alt="" width="0" height="0">
+</a>
+<ul class="main-nav-elements" id="main-nav-elements">
+    <li class="main-nav-element">
+        <a class="nav-page" href="index.html#Welcome">Accueil</a>
+    </li>
+    <li class="main-nav-element main-nav-element-hover">
+        <a href="Presentation.html" class="nav-page">Présentation</a>
+        <ul class="sub-nav-elements" id="sub-nav-elements">
+            <li class="sub-nav-element">
+                <a href="Presentation.html#Who">qui sommes nous</a>
+            </li>
+            <li class="sub-nav-element">
+                <a href="Presentation.html#History">historique du club</a>
+            </li>
+            <li class="sub-nav-element">
+                <a href="Presentation.html#Activities">activités du club</a>
+            </li>
+        </ul>
+    </li>
+    <li class="main-nav-element">
+        <a href="Events.html" class="nav-page">Evènements</a>
+    </li>
+    <li class="main-nav-element">
+        <a href="Contact.css" class="nav-page">Contact</a>
+    </li>
+    <button class="reg-button">S'inscrire<i class="fas fa-arrow-right"></i></button>
+</ul>
+</div>
+`
+document.getElementById("navbar").insertAdjacentHTML("afterbegin",h);
 
-// document.getElementById("navbar").insertAdjacentElement("afterbegin",h);
-$(function(){
-     $("#navbar").load("nav.html");
-  });
+    let x=document.getElementById("navbar");
+    let z=document.getElementById("sub-nav-elements");
+    let y=0;
+    window.addEventListener("scroll",(e)=>{
+        a=document.documentElement.scrollTop
+        if(a>1){
+            x.style.position="fixed"
+        }
+        if(a>"500" && y==0){
+            x.className+=" fix-it"
+            z.className+=" fix-sub"
+            y++
+        }
+        if(a<"500" && y==1){
+            x.className="navbar"
+            z.className="sub-nav-elements"
+            y--;
+        }
+     })
